16b8f547d5db87db1f867bfdc8fd62a80d5b0afb
c17179n <64582595+c17179n@users.noreply.github.com>
2022-04-18 01:43:21 +0300

    Update loader.c

--- loader.c
+++ loader.c
@@ -3,15 +3,15 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 MODULE_LICENSE(“GPL”);
-MODULE_AUTHOR(“Robert W. Oliver II”);
-MODULE_DESCRIPTION(“A simple example Linux module.”);
+MODULE_AUTHOR(“C17179n”);
+MODULE_DESCRIPTION(“dmesq send.”);
 MODULE_VERSION(“0.01”);
-static int __init lkm_example_init(void) {
+static int __init lkm_parser_init(void) {
  printk(KERN_INFO “running module!\n”);
  return 0;
 }fopen dmesq
-static void __exit lkm_example_exit(void) {
+static void __exit lkm_parser_exit(void) {
  printk(KERN_INFO “Goodbye, World!\n”);
 }
-module_init(lkm_example_init);
-module_exit(lkm_example_exit);
+module_init(lkm_parser_init);
+module_exit(lkm_parser_exit);
