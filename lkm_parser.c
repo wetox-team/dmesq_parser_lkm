bfa66b60e053e301a535ce80d469ef5b21f13bdd
c17179n <64582595+c17179n@users.noreply.github.com>
2022-04-18 14:34:54 +0300

    Update and rename loader.c to lkm_parser.c

--- lkm_parser.c
+++ lkm_parser.c
@@ -9,7 +9,10 @@ MODULE_VERSION(“0.01”);
 static int __init lkm_parser_init(void) {
  printk(KERN_INFO “running module!\n”);
  return 0;
-}fopen dmesq
+}
+fopen(var/log/dmesq)
+ fwrite
+
 static void __exit lkm_parser_exit(void) {
  printk(KERN_INFO “Goodbye, World!\n”);
 }
